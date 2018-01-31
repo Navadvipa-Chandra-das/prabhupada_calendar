41ba167175577142ad08aa8cfd6375ddf75c54d9
Navadvipa-Chandra-das <navadwip@rambler.ru>
2018-01-31 17:20:52 +0300

    Ура! Добавили! Харе Кришна!

--- source/Prabhupada_convert.d
+++ source/Prabhupada_convert.d
@@ -85,7 +85,7 @@ class Класс_О_программе : VerticalLayout
     auto гл_Версия = new HorizontalLayout();
 
     гл_Версия.addChild( new TextWidget( "Версия"c,     UIString.fromId( "Версия"c ) ) );
-    гл_Версия.addChild( new TextWidget( "Номер версии"c, "1.0.6"d ).styleId( "POPUP_MENU" ) );
+    гл_Версия.addChild( new TextWidget( "Номер версии"c, "2.0.0"d ).styleId( "POPUP_MENU" ) );
 
     addChild( гл_Версия );
   
